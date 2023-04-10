e962b87be1ad4f17e0090aa525cfbe54c58eef0a
Pablo Margreff <pmargreff@gmail.com>
2023-04-10 11:02:48 -0300

    feature: add packages list reader

---
+++ app/services/package_list_reader.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class PackageListReader
+  attr_accessor :server
+
+  PACKAGES_FILE = "PACKAGES.gz"
+
+  def call(server)
+    @server = server
+
+    packages_path = @server + PACKAGES_FILE
+    content = read_packages(packages_path)
+    parse_packages(content)
+  end
+
+  private
+
+  def read_packages(path)
+    GzipReader.new().call(path)
+  end
+
+  def parse_packages(content)
+    DcfReader.new().call(content)
+  end
+end
---
+++ test/services/package_list_reader_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class PackageListReaderTest < ActiveSupport::TestCase
+  def setup
+    @server = file_fixture("small_server/")
+  end
+
+  test "reads package list from small_server" do
+    result = PackageListReader.new().call(@server)
+
+    assert_equal result.size, 3
+
+    assert_equal result.first["Package"], "A3"
+    assert_equal result.second["Package"], "AalenJohansen"
+    assert_equal result.third["Package"], "AATtools"
+  end
+end
